4cb99b40e622666bc8c0655b422df5c7c0e389f8
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2023-01-29 15:32:40 +0100

    Started recursive construction of successors - working code for lines 0 & 1 of the algo

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-""" PSEUDOCODE
-line    command
------
-i <- processNr
-     for l in range(0,n-1)
-0        levels[i] = l
-1        last_to_enter[l] = i
-2        while last_to_enter[l]=i & there exists k!=i : levels[k]>=l:
-3            wait(i)
-4    critical(i)
-5    levels[i]=-1
-"""
 stateCounter = 0
 
 import copy
@@ -26,8 +14,11 @@ class State:
         self.parent_id = -1
         stateCounter+=1
 
+    def toDot(self):
+        return f"{self.id} [label=\"{self.levels} {self.last_to_enter}\\n{self.process_states}\"]"
+
     def __str__(self):
-        return f"---\nState {self.id} \n\tLevels - {self.levels} \n\tl_t_e - {self.last_to_enter} \n\tpstates - {self.process_states} \n"
+        return f"---\nState {self.id} \n\tLevels - {self.levels} \n\tlte - {self.last_to_enter} \n\tpstates - {self.process_states} \n"
 
 
     def __repr__(self):
@@ -47,7 +38,9 @@ def constructChildState(state:State):
     ns.parent_id = state.id
     ns.levels = copy.deepcopy(state.levels)
     ns.last_to_enter = copy.deepcopy(state.last_to_enter)
-    ns.process_states = copy.deepcopy(state.process_states)
+    ns.process_states = []
+    for ps in state.process_states:
+        ns.process_states.append(copy.deepcopy(ps))  # necessary deepcopy of dict
     return ns
 
 def getSuccessors(state:State):
@@ -57,28 +50,38 @@ def getSuccessors(state:State):
     for i in range(len(state.process_states)):
         processState = state.process_states[i]
         processLine = processState["line"]
-        processIt = processState["it"]
+        l = processState["it"]  # process' iteration counter
         ns = constructChildState(state)
         ns.parent_id = parentId
         if processLine == 0:
-            ns.levels[i] = processIt
+            ns.levels[i] = l  # levels[i] = l
         elif processLine == 1:
-            ns.last_to_enter[processIt] = i
+            ns.last_to_enter[l] = i  # last_to_enter[l] = i
         elif processLine == 2:
             pass
         elif processLine == 3:
             pass
         elif processLine == 4:
             pass
+        elif processLine == 5:
+            pass
         ns.process_states[i]["line"] = processLine + 1
 
         sc.append(ns)
     return sc
 
+def printDot(allStates:dict, allTransitions:dict):
+    print("digraph G {")
+    for s in allStates.values():
+        print(s.toDot())
+    for t in allTransitions:
+        for toState in allTransitions[t]:
+            print(f"{t} -> {toState}")
+    print("}")
 
-if __name__ == '__main__':
-    root = State(3)
-    allStates = {0:root}
+def performBFS():
+    root = State(2)
+    allStates = {0: root}
     allTransitions = dict()  # if 0→[1,2,3] stores {0:[1,2,3]}
     newStates = [root]
     oldSC = stateCounter
@@ -92,9 +95,13 @@ if __name__ == '__main__':
         # ToDo check if new states are duplicate, and if all of them are, break.
         #  Do so by looping over allStates and checking ==? if duplicate, store transition from new state's parent
         #  to found duplicate state
-        if newStates:
+        for s in newStates:
+            allStates[s.id] = s
+            allTransitions[s.parent_id] = allTransitions.get(s.parent_id, []) + [s.id]
+        if stateCounter > 5:
             break
-    for s in newStates:
-        print(s)
-    pass
 
+    printDot(allStates, allTransitions)
+
+if __name__ == '__main__':
+    performBFS()
