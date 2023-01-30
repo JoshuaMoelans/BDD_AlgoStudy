48672ad70f9750b646ab29fe349bf04896fd2e42
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2023-01-30 20:58:16 +0100

    restructure for introducing Lamport

--- main.py
+++ main.py
@@ -4,17 +4,72 @@ stateCounter = 0
 
 import copy
 
-class State:
+class PetersonState:
     def __init__(self, n):
         global stateCounter
         self.n = n
         self.levels = [-1]*n
         self.last_to_enter = [0]*(n-1)
         self.process_states = [{"it":0,"line":0,"status":"exec"}]*n
-        self.children:[State] = []
+        self.children:[PetersonState] = []
         self.id = stateCounter
         self.parent_id = -1
         stateCounter+=1
+
+    def getSuccessors(self):
+        parentId = self.id
+        sc = []
+
+        for i in range(len(self.process_states)):
+            processState = self.process_states[i]
+            prevProcessLine = processState["line"]
+            l = processState["it"]  # process' iteration counter
+            ns = self.constructChildState()
+            ns.parent_id = parentId
+            if prevProcessLine == 0:
+                ns.levels[i] = l  # levels[i] = l
+                ns.process_states[i]["line"] = 1
+            elif prevProcessLine == 1:
+                ns.last_to_enter[l] = i  # last_to_enter[l] = i
+                ns.process_states[i]["line"] = 2
+                ns.process_states[i]["status"] = "wait"
+            elif prevProcessLine == 2:
+                condition = (ns.last_to_enter[l] == i)  # last_to_enter[l] == i
+                kExists = False
+                for k in range(len(ns.levels)):
+                    if (k != i) & (ns.levels[k] >= l):
+                        kExists = True
+                condition = condition & kExists
+                if not condition:  # if while condition is NOT met, we can go to next iteration OR to critical section
+                    if l < ns.n - 2:  # if we are not at the last iteration, we can go to next iteration
+                        ns.process_states[i]["line"] = 0
+                        ns.process_states[i]["it"] = l + 1
+                    else:  # if we are at the last iteration, we can enter the critical section
+                        ns.process_states[i]["status"] = "critical"
+                        ns.process_states[i]["line"] = 4
+                else:  # while condition still holds, we must wait
+                    ns.process_states[i]["status"] = "wait"
+                    ns.process_states[i]["line"] = 2
+            elif prevProcessLine == 4:
+                ns.process_states[i]["line"] = 5
+                ns.process_states[i]["status"] = "exec"
+            elif prevProcessLine == 5:
+                ns.levels[i] = -1  # levels[i] = -1
+                ns.process_states[i]["line"] = 0
+                ns.process_states[i]["it"] = 0
+
+            sc.append(ns)
+        return sc
+
+    def constructChildState(self):
+        ns = PetersonState(self.n)
+        ns.parent_id = self.id
+        ns.levels = copy.deepcopy(self.levels)
+        ns.last_to_enter = copy.deepcopy(self.last_to_enter)
+        ns.process_states = []
+        for ps in self.process_states:
+            ns.process_states.append(copy.deepcopy(ps))  # necessary deepcopy of dict
+        return ns
 
     def simpleString(self):
         out = f"{self.levels} {self.last_to_enter}\\n"
@@ -63,60 +118,6 @@ class State:
     def __hash__(self):
         return 0  # (I think) hash is not used, so we can return 0; implemented to allow set() to work
 
-def constructChildState(state:State):
-    ns = State(state.n)
-    ns.parent_id = state.id
-    ns.levels = copy.deepcopy(state.levels)
-    ns.last_to_enter = copy.deepcopy(state.last_to_enter)
-    ns.process_states = []
-    for ps in state.process_states:
-        ns.process_states.append(copy.deepcopy(ps))  # necessary deepcopy of dict
-    return ns
-
-def getSuccessors(state:State):
-    parentId = state.id
-    sc = []
-
-    for i in range(len(state.process_states)):
-        processState = state.process_states[i]
-        prevProcessLine = processState["line"]
-        l = processState["it"]  # process' iteration counter
-        ns = constructChildState(state)
-        ns.parent_id = parentId
-        if prevProcessLine == 0:
-            ns.levels[i] = l  # levels[i] = l
-            ns.process_states[i]["line"] = 1
-        elif prevProcessLine == 1:
-            ns.last_to_enter[l] = i  # last_to_enter[l] = i
-            ns.process_states[i]["line"] = 2
-            ns.process_states[i]["status"] = "wait"
-        elif prevProcessLine == 2:
-            condition = (ns.last_to_enter[l] == i)  # last_to_enter[l] == i
-            kExists = False
-            for k in range(len(ns.levels)):
-                if (k != i) & (ns.levels[k] >= l):
-                    kExists = True
-            condition = condition & kExists
-            if not condition:  # if while condition is NOT met, we can go to next iteration OR to critical section
-                if l < ns.n-2:  # if we are not at the last iteration, we can go to next iteration
-                    ns.process_states[i]["line"] = 0
-                    ns.process_states[i]["it"] = l + 1
-                else:  # if we are at the last iteration, we can enter the critical section
-                    ns.process_states[i]["status"] = "critical"
-                    ns.process_states[i]["line"] = 4
-            else:  # while condition still holds, we must wait
-                ns.process_states[i]["status"] = "wait"
-                ns.process_states[i]["line"] = 2
-        elif prevProcessLine == 4:
-            ns.process_states[i]["line"] = 5
-            ns.process_states[i]["status"] = "exec"
-        elif prevProcessLine == 5:
-            ns.levels[i] = -1  # levels[i] = -1
-            ns.process_states[i]["line"] = 0
-            ns.process_states[i]["it"] = 0
-
-        sc.append(ns)
-    return sc
 
 def printDot(allStates:set, allTransitions:dict):
     stateIDs = set([s.id for s in allStates])
@@ -173,8 +174,9 @@ class TS:
         with open(f"./output/TS_{self.name}.hoa", "w") as f:
             f.write(outString)
 
-def generatePeterson(n):
-    root = State(n)
+
+def generateTS(n,algo="Peterson"):
+    root = PetersonState(n)
     allStates = {root}
     allTransitions = dict()  # if 0→[1,2,3] stores {0:[1,2,3]}
 
@@ -184,7 +186,7 @@ def generatePeterson(n):
         oldStates = newStates  # BFS expansion
         newStates = []
         for state in oldStates:
-            newStates += getSuccessors(state)  # add all successors to newStates
+            newStates += state.getSuccessors()  # add all successors to newStates
 
         filteredStates = []
         for s in newStates:
@@ -213,7 +215,7 @@ def generatePeterson(n):
 
 if __name__ == '__main__':
     start = time.time()
-    P = generatePeterson(n=2)
+    P = generateTS(n=2,algo="Peterson")
     P.toDot()
     P.toHOA()
     end = time.time()
